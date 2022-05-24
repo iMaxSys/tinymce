2a5fae8d9f7ad5cfd58710bf61bae85e61f075f2
taojy <taox@live.com>
2022-05-24 22:35:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TinymceEditor from "./components/tinymce-editor/index.vue";
+</script>
+
+<template>
+  <div>
+    <TinymceEditor></TinymceEditor>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/tinymce-editor/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import "./tinymce.min.js";
+import "./themes/silver/theme.min.js";
+import "./icons/default/icons.min.js";
+import "./models/dom/model.min.js";
+import "./plugins/preview/plugin.min.js";
+import "./plugins/table/plugin.min.js";
+import "./langs/zh-Hans.js";
+onMounted(() => {
+  tinymce.init({
+    selector: "#tinymce-editor",
+    language: "zh-Hans",
+    base_url: "/tinymce",
+    plugins: "preview table",
+  });
+});
+</script>
+
+<template>
+  <section id="tinymce-editor">it's me.</section>
+</template>
+
+<style lang="scss" scoped>
+@import url("./skins/ui/oxide/skin.min.css");
+</style>
