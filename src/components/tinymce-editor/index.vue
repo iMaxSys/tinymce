46dfab3da9f3ed6b7c4f76afe868c247ad166448
taojy <taox@live.com>
2022-05-25 10:10:50 +0800

    plugin

--- src/components/tinymce-editor/index.vue
+++ src/components/tinymce-editor/index.vue
@@ -13,12 +13,19 @@ onMounted(() => {
     language: "zh-Hans",
     base_url: "/tinymce",
     plugins: "preview table",
+    branding: false,
+    contextmenu: "bold copy ",
+    contextmenu_never_use_native: true,
+    custom_ui_selector: '.my-custom-button',
+    elementpath: false,
+    noneditable_editable_class: "mceNonEditable"
   });
 });
 </script>
 
 <template>
-  <section id="tinymce-editor">it's me.</section>
+  <section id="tinymce-editor">it's me.<strong class="mceNonEditable">不能编辑我哦</strong></section>
+  <button class="my-custom-button">Button</button>
 </template>
 
 <style lang="scss" scoped>
